069fdb487a873283d468d7b86b88ef0f16cfc70b
NagaPranavi9 <nagapranavimaley9@gmail.com>
2024-08-07 04:24:20 +0530

    added cors and path to index and firm controller

--- index.js
+++ index.js
@@ -5,14 +5,14 @@
      const bodyParser = require('body-parser');
      const firmRoutes = require('./routes/firmRoutes'); 
      const productRoutes = require('./routes/productsRoutes');
-   //   const cors = require('cors')
+     const cors = require('cors')
      const path = require('path')
 
      const app = express()
      const PORT= process.env.PORT || 4000;
 
      dotEnv.config();
-   //   app.use(cors())
+     app.use(cors())
 
      mongoose.connect(process.env.MONGO_URI)
      .then(()=>console.log("Mongodb connected successfully!"))
